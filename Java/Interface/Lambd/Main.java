1655d2f8e962046087870796a46dee2000846e58
HyeBin, Park <35947674+HYEBPARK@users.noreply.github.com>
2022-03-23 15:42:00 +0900

    제너릭 T

--- Java/Interface/Lambd/Main.java
+++ Java/Interface/Lambd/Main.java
@@ -5,17 +5,20 @@ public class Main {
 
         MySupplier<String> s = () -> "Hello World";
 
-        // MyMapper m = (str) -> str.length();
         MyMapper<String, Integer> m = String::length; // method reference
         MyMapper<Integer, Integer> m2 = i -> i * i;
         MyMapper<Integer, String> m3 = Integer::toHexString;
-        // MyConsumer c = (i) -> System.out.println(i);
-        MyConsumer<Integer> c = System.out::println; // method reference
-       
-        MyRunnable r = () -> c.consume(m.map(s.supply()));
+        MyConsumer<String> c = System.out::println; // method reference
 
+        MyRunnable r = () -> c.consume(
+                m3.map(
+                        m2.map(
+                                m.map(
+                                        s.supply()
+                                )
+                        )
+                )
+        );
         r.run();
-
-
     }
 }
